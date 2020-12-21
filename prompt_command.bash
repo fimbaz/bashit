c4cf43701d8b5f350adf7e89e17045f126992283
Matthew Charles Baker <mbaker@subspace.com>
2020-12-20 22:31:29 -0800

    Add a prompt command that print each command  to the terminal then blanks it out.

--- prompt_command.bash
+++ prompt_command.bash
@@ -1,4 +1,7 @@
-PROMPT_COMMAND='X=$(history 1 | tr n " ");SPACES=$(printf %${#X}s);BSES=$(printf %${#X}s |'" tr ' ' $'\b');"'echo -n $(history 1 |'" tr $'\n' ' ')"
+PROMPT_COMMAND='X=$(history 1);SPACES="$(printf %${#X}s)";BSES=$(printf %${#X}s |'" tr ' ' $'\b');"'echo -n "${X}"; echo -n "${BSES}${SPACES}${BSES}"'
+#'echo -n "${BSES}${SPACES}${BSES};"'
+
+#PROMPT_COMMAND='X=$(history 1 | tr n " ");SPACES=$(printf %${#X}s);BSES=$(printf %${#X}s |'" tr ' ' $'\b');"'echo -n $(history 1 |'" tr $'\n' ' ')"
 
 
 #BSES=$(printf %${#X}s | tr ' ' $'\b')"
