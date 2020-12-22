0b93cb1dea1a7951a5ab603b02a5e76683093aae
Matthew Charles Baker <mbaker@subspace.com>
2020-12-21 16:40:33 -0800

    not working

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use regex::Regex;
+use resiter::while_ok::*;
+use std::io::{self, BufRead, Read};
+
+fn main() -> io::Result<()> {
+    let re = Regex::new(r"\(\(MBHIST\)\)(.*)\(\(MBHISTEND\)\)").unwrap();
+    let mut buffer = String::new();
+    let mut stdin = io::stdin(); // We get `Stdin` here.
+    for maybe_line in stdin.lock().lines() {
+        match maybe_line {
+            Ok(line) => {
+                match re.captures(&line) {
+                    Some(capture) => {
+                        let history_item = capture.get(1).unwrap().as_str();
+                        println!("{}", history_item);
+                    }
+                    None => continue,
+                }
+                println!("{}", re.captures(&line).unwrap().get(1).unwrap().as_str());
+            }
+            Err(_) => return Ok(()),
+        }
+    }
+
+    Ok(())
+}
+//	 if let Some(captures) = re.captures(&line){
