2aa3b09324e7328787b5224ffc86baa3be62ee56
Matthew Charles Baker <mbaker@subspace.com>
2021-01-01 13:53:19 -0800

    disable exit on error so we can reset terminal after ssh is done

--- bashit.bash
+++ bashit.bash
@@ -3,6 +3,7 @@ DIR=/usr/local/bin/
 rm -f ~/comm
 mkfifo ~/comm
 stty -echo raw
+set +e
 cat -u ~/comm | ssh -tt "$@" $(cat $DIR/prompt_command.bash) bash -i | tee >($DIR/bashit >> $HOME/.bash_universal_history)&
 cat -u - < /dev/stdin > ~/comm
 reset
