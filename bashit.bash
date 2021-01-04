c608d165b1a3e547544b9c9b83be700c29097186
Matthew Charles Baker <mbaker@subspace.com>
2021-01-04 14:32:48 -0800

    trying a thing

--- bashit.bash
+++ bashit.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 DIR=/usr/local/bin/
-rm -f ~/comm
-mkfifo ~/comm
+mkdir -p ~/.bashit
+rm -f ~/.bashit/comm ~/.bashit/cmd
+mkfifo ~/.bashit/cmd ~/.bashit/comm
 stty -echo raw
 set +e
-cat -u ~/comm | ssh -tt "$@" $(cat $DIR/prompt_command.bash) bash -i | tee >($DIR/bashit >> $HOME/.bash_universal_history)&
-cat -u - < /dev/stdin > ~/comm
+cat -u ~/.bashit/comm | ssh -tt "$@" $(cat $DIR/prompt_command.bash) bash -i | tee >($DIR/bashit >> $HOME/.bash_universal_history)&
+bash -c "cat - < ~/.bashit/cmd | bash | > ~/.bashit/comm"&
+cat -u - < /dev/stdin > ~/.bashit/comm
 reset
