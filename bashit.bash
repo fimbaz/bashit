5ebfcc8e76a96fd0f994a6d59e80c7c64546f11e
Matthew Charles Baker <mbaker@subspace.com>
2021-01-04 19:09:18 -0800

    relentlessness

--- bashit.bash
+++ bashit.bash
@@ -5,7 +5,19 @@ rm -f ~/.bashit/comm ~/.bashit/cmd
 mkfifo ~/.bashit/cmd ~/.bashit/comm
 stty -echo raw
 set +e
-cat -u ~/.bashit/comm | ssh -tt "$@" $(cat $DIR/prompt_command.bash) bash -i | tee >($DIR/bashit >> $HOME/.bash_universal_history)&
+cat -u ~/.bashit/comm | ssh -tt "$@" $(cat $DIR/prompt_command.bash) bash -i | \
+    tee >( exec 3<> ~/.bashit/cmd;
+           while read CMD; do
+               [[ $CMD =~ \(\(MBHIST\)\)(.*)\(\(MBHISTEND\)\) ]]
+               if [[  ${BASH_REMATCH[1]} != "" ]]; then
+                   echo ${BASH_REMATCH[1]} >> ~/.bash_universal_history
+               fi
+               [[ $CMD =~ \(\(MBCMD\)\)(.*)\(\(MBCMDEND\)\) ]]
+               if [[  ${BASH_REMATCH[1]} != "" ]]; then
+                   echo $(eval ${BASH_REMATCH[1]}) >&3
+               fi
+           done)&
+#tee >($DIR/bashit >> $HOME/.bash_universal_history)&
 bash -c "cat - < ~/.bashit/cmd | bash | > ~/.bashit/comm"&
 cat -u - < /dev/stdin > ~/.bashit/comm
 reset
